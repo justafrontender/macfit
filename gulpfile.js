7a95e169ae8ce577d0e0991b123b0e660cb5d284
Roman <roman@Mac-mini-Roman.local>
2017-12-21 20:01:12 +0300

    Удаление лишнего из gulpfile и перенос сборки в папку static

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
-'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var server = require('browser-sync').create();
+const sequence = require('run-sequence');
+const del = require('del');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const svgmin = require('gulp-svgmin');
+const svgstore = require('gulp-svgstore');
 
-var sequence = require('run-sequence');
-var del = require('del');
-var csso = require('gulp-csso');
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var svgmin = require('gulp-svgmin');
-var svgstore = require('gulp-svgstore');
-var ghPages = require('gulp-gh-pages');
-
-gulp.task('build', function(fn) {
+gulp.task('build', fn => {
   sequence(
     'clean',
-    'copy',
     'style',
     'images',
     'svg',
@@ -28,25 +23,7 @@ gulp.task('build', function(fn) {
   );
 });
 
-gulp.task('deploy', function() {
-  return gulp.src('build/**/*')
-    .pipe(ghPages());
-});
-
-gulp.task('serve', function() {
-  server.init({
-    server: 'build/',
-    notify: false,
-    open: false,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch('sass/**/*.{scss,sass}', ['style']);
-  gulp.watch('*.html', ['html:update']);
-});
-
-gulp.task('style', function() {
+gulp.task('style', () => {
   gulp.src('sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
@@ -58,62 +35,37 @@ gulp.task('style', function() {
         ]
       })
     ]))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('static/css'))
     .pipe(csso())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css'))
-    .pipe(server.stream());
+    .pipe(gulp.dest('static/css'));
 });
 
-gulp.task('copy', function() {
-  return gulp.src(
-    [
-      'fonts/**/*.{woff,woff2}',
-      'js/**',
-      '*.html'
-    ],
-    {
-      base: '.'
-    }
-  )
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('images', function() {
+gulp.task('images', () => {
   gulp.src('img/**/*.{png,jpg,gif}')
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true })
     ]))
-    .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest('static/img'));
 });
 
-gulp.task('svg', function() {
+gulp.task('svg', () => {
   gulp.src('img/**/*.svg')
     .pipe(svgmin())
-    .pipe(gulp.dest('build/img'))
+    .pipe(gulp.dest('static/img'));
 });
 
-gulp.task('sprite', function() {
+gulp.task('sprite', () => {
   gulp.src('img/{icon,logo}-*.svg')
     .pipe(svgmin())
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest('static/img'));
 });
 
-gulp.task('clean', function() {
-  return del('build');
-});
-
-gulp.task('html:copy', function() {
-  return gulp.src('*.html')
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('html:update', ['html:copy'], function(done) {
-  server.reload();
-  done();
+gulp.task('clean', () => {
+  return del('static');
 });
