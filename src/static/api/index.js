bdf0fbfbebc60613093c386a3117a902c43413a6
Babaev Roman <kotfly@gmail.com>
2017-12-23 01:46:27 +0300

    Импорт статичной заглушки бэкенда в /api

--- src/static/api/index.js
+++ src/static/api/index.js
@@ -1,3 +1,2 @@
-const req = require.context(`!file-loader?name=api/[name]!./`, false, /[^(\.\/index)]/);
-console.log(req.keys());
+const req = require.context(`!file-loader?name=api/[name]!./`, false, /^\.\/[a-zA-Z0-9-]+[^index(.js)?]$/);
 req.keys().map(file => req(file));
