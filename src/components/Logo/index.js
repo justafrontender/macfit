62418045eea8cbc1bd366cf463fe10af018d0327
Babaev Roman <kotfly@gmail.com>
2017-12-27 00:21:57 +0300

    Подключение svg-картинок из дизайна

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'react-router-dom/Link';
-import LogoImg from '../../../img/logo.svg';
+import LogoImg from './img/logo.svg';
 
 const Logo = () => (
   <Link className='logo' to='/'>
