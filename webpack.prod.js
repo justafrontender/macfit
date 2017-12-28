8c2732036c3802a05dc883eee4117744d88ff981
Roman <roman@Mac-mini-Roman.local>
2017-12-28 18:43:16 +0300

    Убрал sourcemap из production сборки

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  devtool: 'none',
+
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
