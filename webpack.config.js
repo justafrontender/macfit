6139c86fa5efea05dd203748ba7551e4a443aaa2
Roman <roman@Mac-mini-Roman.local>
2017-12-28 19:12:30 +0300

    Набор правок: Исправление баги с импортом одновременно dev и prod конфигов. Подключение webpack-bundle-analyzer.

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const dev = require('./webpack.dev.js');
-const prod = require('./webpack.prod.js');
-
-module.exports = merge(common, process.env.NODE_ENV === 'production' ? prod : dev);
