b7118e377f9518159ab739d7b330ca639bcc905d
Roman <roman@Mac-mini-Roman.local>
2018-02-15 16:02:43 +0300

    Исправляет метод запроса

--- src/server/orders.js
+++ src/server/orders.js
@@ -1,7 +1,7 @@
 export default {
   create() {
     return new Promise((resolve, reject) => {
-      fetch('/api/orders', { method: 'PUT', body: '' })
+      fetch('/api/orders', { method: 'POST', body: '' })
         .then(response => response.json())
         .then(obj => {
           resolve(obj);
