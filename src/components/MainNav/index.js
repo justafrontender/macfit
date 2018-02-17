7c50ae65d9d3d4b48e6f312ecdff6429d82d3df5
Babaev Roman <kotfly@gmail.com>
2018-02-18 01:24:36 +0300

    Закрытие MainNav по клику вне элемента

--- src/components/MainNav/index.js
+++ src/components/MainNav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import NavLink from 'react-router-dom/NavLink';
 import PropTypes from 'prop-types';
 import b from '../../lib/b';
@@ -6,55 +7,71 @@ import hash from '../../lib/hash';
 import Counter from '../Counter';
 import './style.scss';
 
-const MainNav = ({ menuOpened, menuItems, counter, onClick }) => {
-  return (
-    <nav className={b('main-nav')}>
-      <ul className={b('main-nav__list', menuOpened && 'visible')}>
-        {
-          !!counter &&
+class MainNav extends React.Component {
+  componentWillUpdate(nextProps) {
+    if (nextProps.isOpened && !this.props.isOpened) {
+      document.addEventListener('click', this.onExternalClick);
+    }
+    if (!nextProps.isOpened && this.props.isOpened) {
+      document.removeEventListener('click', this.onExternalClick);
+    }
+  }
+
+  onExternalClick = evt => {
+    if (!findDOMNode(this).contains(evt.target)) this.props.onClose();
+  }
+
+  render() {
+    const { isOpened, menuItems, counter, onClose } = this.props;
+    return (
+      <nav className={b('main-nav')}>
+        <ul className={b('main-nav__list', isOpened && 'visible')}>
+          {
+            !!counter &&
+            <li>
+              <NavLink
+                className='main-nav__link'
+                activeClassName='main-nav__link--active'
+                to='/order'
+                onClick={onClose}
+              >
+                Ваш заказ
+                <Counter number={counter} classMix='main-nav__counter' />
+              </NavLink>
+            </li>
+          }
           <li>
             <NavLink
               className='main-nav__link'
               activeClassName='main-nav__link--active'
-              to='/order'
-              onClick={onClick}
+              to='/'
+              exact
+              onClick={onClose}
             >
-              Ваш заказ
-              <Counter number={counter} classMix='main-nav__counter' />
+              Еда
             </NavLink>
           </li>
-        }
-        <li>
-          <NavLink
-            className='main-nav__link'
-            activeClassName='main-nav__link--active'
-            to='/'
-            exact
-            onClick={onClick}
-          >
-            Еда
-          </NavLink>
-        </li>
-        {menuItems.map(link => (
-          <li key={hash(link.name)}>
-            <NavLink
-              className='main-nav__link'
-              activeClassName='main-nav__link--active'
-              to={link.href}
-              onClick={onClick}
-            >
-              {link.name}
-              {link.counter ? <Counter number={counter} classMix='main-nav__counter' /> : ''}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+          {menuItems.map(link => (
+            <li key={hash(link.name)}>
+              <NavLink
+                className='main-nav__link'
+                activeClassName='main-nav__link--active'
+                to={link.href}
+                onClick={onClose}
+              >
+                {link.name}
+                {link.counter ? <Counter number={counter} classMix='main-nav__counter' /> : ''}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+}
 
 MainNav.propTypes = {
-  menuOpened: PropTypes.bool,
+  isOpened: PropTypes.bool,
   menuItems: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string,
     href: PropTypes.string,
