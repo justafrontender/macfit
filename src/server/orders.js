c4590bda4864056cb728c7aa3b269c7c3401f790
Babaev Roman <kotfly@gmail.com>
2018-02-17 04:06:28 +0300

    Исправляет метод создания заказа и экспорт методов server

--- src/server/orders.js
+++ src/server/orders.js
@@ -1,7 +1,7 @@
 export default {
-  create() {
+  create(payload) {
     return new Promise((resolve, reject) => {
-      fetch('/api/orders', { method: 'POST', body: '' })
+      fetch('/api/orders', { method: 'POST', body: payload })
         .then(response => response.json())
         .then(obj => {
           resolve(obj);
